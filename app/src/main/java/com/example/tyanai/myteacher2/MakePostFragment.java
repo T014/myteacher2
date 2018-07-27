274e5682c788c2e1362c4f6179e390c3e02ce6d5
T014 <t014.ty@ex.fyf-h.com>
2018-07-27 15:20:03 +0900

    ドロワー追加 profileActivity profileのfragment作成中 アイコン画像ヘッダー画像読み込んだ プロフィールデータを送信 アクションバーの切り替え ドロワーの実装 ドロワーメニューの追加 ログアウト画面実装 利用規約画面実装 searchButton追加 データの取得できた プロフィール画像取得 投稿分野単一選択
    投稿分野改行
    プロフィールデータの読み込み
    データ保存上書きから更新に
    アカウント作成時にperiodに日時を入れる
    アカウントを表示するときはツールバーを非表示
    MainActivityからinputprofileの画像選択しようとすると
    Profileactivityのメソッドを使いたくてクラッシュする

--- app/src/main/java/com/example/tyanai/myteacher2/MakePostFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/MakePostFragment.java
@@ -6,13 +6,34 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MakePostFragment extends Fragment {
     public static final String TAG = "MakePostFragment";
+
+
+    private ImageView postImageView;
+    private EditText postEditText;
+    private EditText dateEditText;
+    private EditText methodEditText;
+    private EditText costEditText;
+    private EditText longEditText;
+    private EditText careerEditText;
+    private RadioGroup areaGroup;
+    private RadioGroup sportsGroup;
+    private Button sendButton;
+    private String area;
 
 
     @Override
@@ -20,6 +41,17 @@ public class MakePostFragment extends Fragment {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_makepost,container,false);
 
+        postImageView = (ImageView)v.findViewById(R.id.postImageView);
+        postEditText = (EditText)v.findViewById(R.id.postEditText);
+        dateEditText = (EditText)v.findViewById(R.id.dateEditText);
+        methodEditText = (EditText)v.findViewById(R.id.methodEditText);
+        costEditText = (EditText)v.findViewById(R.id.costEditText);
+        longEditText = (EditText)v.findViewById(R.id.longEditText);
+        careerEditText = (EditText)v.findViewById(R.id.careerEditText);
+        sendButton = (Button)v.findViewById(R.id.sendButton);
+
+        areaGroup = (RadioGroup)v.findViewById(R.id.areaRadioGroup);
+        sportsGroup = (RadioGroup)v.findViewById(R.id.sportsRadioGroup);
 
 
 
@@ -27,75 +59,88 @@ public class MakePostFragment extends Fragment {
         return v;
     }
 
-    public void onViewCreated(View view,Bundle savedInstanceState) {
+    public void onViewCreated(final View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-/*
+
+        sportsGroup.setVisibility(View.GONE);
+        area = "";
 
 
-        ActionBar actionBar = getActivity().getActionBar();
-        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
-        actionBar.addTab(actionBar
-                .newTab()
-                .setText("PR")
-                .setTabListener(
-                        new MainTabListener<ConfirmProfileFragment>(
-                                getActivity(),
-                                "f1",
-                                ConfirmProfileFragment.class
-                        )
-                ));
-        actionBar.addTab(actionBar
-                .newTab()
-                .setText("募集")
-                .setTabListener(
-                        new MainTabListener<ContractFragment>(
-                                this,
-                                "f2",
-                                ContractFragment.class
-                        )
-                ));
+
+        areaGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int checkedId) {
+
+                if (checkedId != -1){
+                    //選択されているボタンの取得
+                    RadioButton selectedRadioButton = (RadioButton)view.findViewById(checkedId);
+                    String selectedArea = selectedRadioButton.getText().toString();
+
+                    if (selectedArea.equals("スポーツ")){
+                        //スポーツの一覧を表示する
+                        sportsGroup.setVisibility(View.VISIBLE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("音楽")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("動画")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("学習")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("料理")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("手芸")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("芸術")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("漫画")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }else if(selectedArea.equals("その他")){
+                        sportsGroup.setVisibility(View.GONE);
+                        area=selectedArea;
+                    }
+
+                }else{
+                    //何も選択されていない
+                    //すべての一覧を非表示
+                    sportsGroup.setVisibility(View.GONE);
+                }
+
+            }
+        });
+
+        sendButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                //必須事項が入力されているかの確認
+
+
+//
+//                String favorites = "未設定";
+//
+//                Map<String,Object> data = new HashMap<>();
+//
+//
+//                data.put("headerBitmapString", headerBitmapString);
+//
+//                Map<String,Object> childUpdates = new HashMap<>();
+//                childUpdates.put(userId,data);
+//                userRef.updateChildren(childUpdates);
+            }
+        });
+
+
 
 
 
     }
-    public static class MainTabListener<T extends Fragment> implements ActionBar.TabListener {
-        private Fragment fragment;
-        private final Activity activity;
-        private final String tag;
-        private final Class<T> cls;
 
-        public MainTabListener(
-                Activity activity, String tag, Class<T> cls){
-            this.activity = activity;
-            this.tag = tag;
-            this.cls = cls;
-
-        }
-
-        @Override
-        public void onTabReselected(Tab tab, FragmentTransaction ft) {
-        }
-
-        @Override
-        public void onTabSelected(Tab tab, FragmentTransaction ft) {
-            if(fragment == null){
-                fragment = Fragment.instantiate(activity, cls.getName());
-                ft.add(android.R.id.content, fragment, tag);
-            }
-            else{
-                ft.attach(fragment);
-            }
-        }
-
-        @Override
-        public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-            if(fragment != null){
-                ft.detach(fragment);
-            }
-        }
-
-*/
-}
 
 }
