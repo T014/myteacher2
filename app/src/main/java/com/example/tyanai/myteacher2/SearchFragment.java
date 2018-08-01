74e31b2e4a3b428759bd007c4fa2577f1b7d289e
T014 <t014.ty@ex.fyf-h.com>
2018-08-01 17:18:28 +0900

    ドロワー追加 profileActivity profileのfragment作成中 アイコン画像ヘッダー画像読み込んだ プロフィールデータを送信 アクションバーの切り替え ドロワーの実装 ドロワーメニューの追加 ログアウト画面実装 利用規約画面実装 searchButton追加 データの取得できた プロフィール画像取得 投稿分野単一選択 投稿送信 タイムライン追加 分野分imagebutton配置 detailsfragment追加 searchfragment追加」 ユーザーデータ、ポストデータ変更 searchfragmentの条件分岐
    投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
    投稿分野改行
    AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
    データ保存上書きから更新に
    アカウント作成時にperiodに日時を入れる
    アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/SearchFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/SearchFragment.java
@@ -8,6 +8,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Spinner;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SearchFragment extends Fragment {
     public static final String TAG = "SearchFragment";
     Spinner postAreaSpinner;
@@ -24,14 +33,108 @@ public class SearchFragment extends Fragment {
     Spinner ageSpinner;
     Button searchButton;
 
-    String level;
-    String userEvaluation;
+    String postLevel;
+    String postUserEvaluation;
+    String postEvaluation;
     String taught;
     String method;
     String date;
     String cost;
     String sex;
     String age;
+
+    DatabaseReference mDataBaseReference;
+    DatabaseReference gridRef;
+    private ArrayList<PostData> searchArrayList = new ArrayList<PostData>();
+
+
+
+
+
+
+    private ChildEventListener sEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            String userId = (String) map.get("userId");
+            String userName = (String) map.get("userName");
+            String time = (String) map.get("time");
+            String key = (String) map.get("key");
+            String date = (String) map.get("date");
+            String imageBitmapString = (String) map.get("imageBitmapString");
+            String contents = (String) map.get("contents");
+            String cost = (String) map.get("cost");
+            String howLong = (String) map.get("howLong");
+            String goods = (String) map.get("goods");
+            String share = (String) map.get("share");
+            String bought = (String) map.get("bought");
+            String evaluation = (String) map.get("evaluation");
+            String cancel = (String) map.get("cancel");
+            String method = (String) map.get("method");
+            String postArea = (String) map.get("postArea");
+            String postType = (String) map.get("postType");
+            String level = (String) map.get("level");
+            String career = (String) map.get("career");
+            String place = (String) map.get("place");
+            String sex = (String) map.get("sex");
+            String age = (String) map.get("age");
+            String taught = (String) map.get("taught");
+            String userEvaluation = (String) map.get("userEvaluation");
+
+
+
+            PostData postData = new PostData(userId,userName,time,key,date,imageBitmapString
+                    , contents,cost,howLong,goods,share,bought,evaluation,cancel,method,postArea
+                    , postType,level,career,place,sex,age,taught,userEvaluation);
+
+
+            //条件分岐
+            int iUserEvaluation = Integer.parseInt(userEvaluation);
+            int iSelectedUserEvaluation = Character.getNumericValue(postUserEvaluation.charAt(0));
+
+            int iPostEvaluation = Integer.parseInt(evaluation);
+            int iSelectedPostEvaluation = Character.getNumericValue(postEvaluation.codePointAt(0));
+
+            if (postLevel.equals("指定しない") || level.equals(postLevel)){
+                if (userEvaluation.equals("指定しない") || iUserEvaluation>iSelectedUserEvaluation){
+                    if(postEvaluation.equals("指定しない") || iPostEvaluation>iSelectedPostEvaluation){
+                        searchArrayList.add(postData);
+
+                        //ここに条件を追加する
+                    }
+                }
+            }
+
+
+
+
+        }
+
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+
+
+        }
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+        }
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+
+
+
+
+    };
+
+
+
+
 
 
     @Override
@@ -64,13 +167,16 @@ public class SearchFragment extends Fragment {
 
         MainActivity.mToolbar.setTitle("探す");
 
+
+
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String postArea = (String)postAreaSpinner.getSelectedItem();
                 String postType = (String)postTypeSpinner.getSelectedItem();
-                level = (String)levelSpinner.getSelectedItem();
-                userEvaluation = (String)userEvaluationSpinner.getSelectedItem();
+                postLevel = (String)levelSpinner.getSelectedItem();
+                postUserEvaluation = (String)userEvaluationSpinner.getSelectedItem();
+                postEvaluation = (String)evaluationSpinner.getSelectedItem();
                 taught = (String)taughtSpinner.getSelectedItem();
                 method = (String)methodSpinner.getSelectedItem();
                 date = (String)dateSpinner.getSelectedItem();
@@ -78,8 +184,18 @@ public class SearchFragment extends Fragment {
                 sex = (String)sexSpinner.getSelectedItem();
                 age = (String)ageSpinner.getSelectedItem();
 
+                String refArea="";
+                String refType="";
 
-
+                if (postArea.equals("スポーツ")){
+                    refArea = "sports";
+                }
+                if (postType.equals("テニス")){
+                    refType = "tennis";
+                }
+                mDataBaseReference = FirebaseDatabase.getInstance().getReference();
+                gridRef = mDataBaseReference.child(Const.AreaPATH).child(refArea).child(refType);
+                gridRef.addChildEventListener(sEventListener);
 
 
             }
