1a7b6b8d7bdccd75f10a8f6a7a64959d6b787a4c
T014 <t014.ty@ex.fyf-h.com>
2018-08-02 14:25:04 +0900

    ドロワー追加 profileActivity profileのfragment作成中 アイコン画像ヘッダー画像読み込んだ プロフィールデータを送信 アクションバーの切り替え ドロワーの実装 ドロワーメニューの追加 ログアウト画面実装 利用規約画面実装 searchButton追加 データの取得できた プロフィール画像取得 投稿分野単一選択 投稿送信 タイムライン追加 分野分imagebutton配置 detailsfragment追加 searchfragment追加」 ユーザーデータ、ポストデータ変更 searchfragmentの条件分岐 検索機能拡張
    投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
    投稿分野改行
    AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
    データ保存上書きから更新に
    アカウント作成時にperiodに日時を入れる
    アカウントを表示するときはツールバーを非表示

---
+++ app/src/main/java/com/example/tyanai/myteacher2/CustomDialogFragment.java
@@ -0,0 +1,51 @@
+package com.example.tyanai.myteacher2;
+
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
+
+public class CustomDialogFragment extends DialogFragment {
+
+    String dateStr="";
+
+    // ダイアログが生成された時に呼ばれるメソッド ※必須
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        // 今日の日付のカレンダーインスタンスを取得
+        final Calendar calendar = Calendar.getInstance();
+
+        // ダイアログ生成  DatePickerDialogのBuilderクラスを指定してインスタンス化します
+        DatePickerDialog dateBuilder = new DatePickerDialog(
+                getActivity(),
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                        // 選択された年・月・日を整形 ※月は0-1なので+1している
+
+
+                        if (month+1<10 && dayOfMonth<10){
+                            dateStr = year + "/" + ("0"+(month + 1)) + "/" + ("0"+dayOfMonth);
+                        }else if (month+1<10 && dayOfMonth>9){
+                            dateStr = year + "/" + ("0"+(month + 1)) + "/" + (dayOfMonth);
+                        }else if(month+1>9 && dayOfMonth<10){
+                            dateStr = year + "/" + (month + 1) + "/" + ("0"+dayOfMonth);
+                        }else if(month+1>9 && dayOfMonth>9){
+                            dateStr = year + "/" + (month + 1) + "/" + (dayOfMonth);
+                        }
+
+
+                        MakePostFragment.dateTextView.setText(dateStr);
+                    }
+                },
+                calendar.get(Calendar.YEAR), // 初期選択年
+                calendar.get(Calendar.MONTH), // 初期選択月
+                calendar.get(Calendar.DAY_OF_MONTH) // 初期選択日
+        );
+
+        // dateBulderを返す
+        return dateBuilder;
+    }
+}
