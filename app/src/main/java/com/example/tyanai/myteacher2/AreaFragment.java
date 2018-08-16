084b9b9059e2e1dc753a55e97b3e185b107c6069
T014 <t014.ty@ex.fyf-h.com>
2018-08-16 12:52:36 +0900

    ドロワー追加 profileActivity profileのfragment作成中 アイコン画像ヘッダー画像読み込んだ プロフィールデータを送信 アクションバーの切り替え ドロワーの実装 ドロワーメニューの追加 ログアウト画面実装 利用規約画面実装 searchButton追加 データの取得できた プロフィール画像取得 投稿分野単一選択 投稿送信 タイムライン追加 分野分imagebutton配置 detailsfragment追加 searchfragment追加」 ユーザーデータ、ポストデータ変更 searchfragmentの条件分岐 検索機能拡張 タイムライン実装 follow,follower follow,follower開いたときにドロワー閉じる follow,follower数表示 検索結果表示 follow,follower調整中
    投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
    投稿分野改行
    AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
    データ保存上書きから更新に
    アカウント作成時にperiodに日時を入れる
    アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/AreaFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/AreaFragment.java
@@ -10,19 +10,44 @@ import com.google.firebase.auth.FirebaseAuth;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 //activityloginのボタンとかテキストを非表示にする
 
 public class AreaFragment extends Fragment {
     public static final String TAG = "AreaFragment";
 
+    private LinearLayout areaGroup;
+    private LinearLayout sportsGroup;
+    private LinearLayout musicGroup;
+    private LinearLayout movieGroup;
+    private LinearLayout studyGroup;
+    private LinearLayout cookGroup;
 
     private ImageButton sportsImageButton;
     private ImageButton musicImageButton;
     private ImageButton movieImageButton;
     private ImageButton studyImageButton;
 
+    private ImageButton tennisImageButton;
+    private ImageButton soccerImageButton;
+    private ImageButton athleticsImageButton;
+    private ImageButton swimImageButton;
 
+    private ImageButton singImageButton;
+    private ImageButton instrumentImageButton;
+    private ImageButton vocalPercussionImageButton;
+    private ImageButton rapImageButton;
+
+    private ImageButton filmImageButton;
+    private ImageButton editImageButton;
+
+    private ImageButton japaneseImageButton;
+    private ImageButton mathematicsImageButton;
+    private ImageButton englishImageButton;
+    private ImageButton scienceImageButton;
+    private ImageButton societyImageButton;
+    private ImageButton historyImageButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,11 +55,36 @@ public class AreaFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_area,container,false);
 
 
+        areaGroup = (LinearLayout)v.findViewById(R.id.areaGroup);
+        sportsGroup = (LinearLayout)v.findViewById(R.id.sportsGroup);
+        musicGroup = (LinearLayout)v.findViewById(R.id.musicGroup);
+        movieGroup = (LinearLayout)v.findViewById(R.id.movieGroup);
+        studyGroup = (LinearLayout)v.findViewById(R.id.studyGroup);
 
         sportsImageButton = (ImageButton)v.findViewById(R.id.sportsImageButton);
         musicImageButton = (ImageButton)v.findViewById(R.id.musicImageButton);
         movieImageButton = (ImageButton)v.findViewById(R.id.movieImageButton);
         studyImageButton = (ImageButton)v.findViewById(R.id.studyImageButton);
+
+        tennisImageButton = (ImageButton)v.findViewById(R.id.tennisImageButton);
+        soccerImageButton = (ImageButton)v.findViewById(R.id.soccerImageButton);
+        athleticsImageButton = (ImageButton)v.findViewById(R.id.athleticsImageButton);
+        swimImageButton = (ImageButton)v.findViewById(R.id.swimImageButton);
+
+        singImageButton = (ImageButton)v.findViewById(R.id.singImageButton);
+        instrumentImageButton = (ImageButton)v.findViewById(R.id.instrumentImageButton);
+        vocalPercussionImageButton = (ImageButton)v.findViewById(R.id.vocalPercussionImageButton);
+        rapImageButton = (ImageButton)v.findViewById(R.id.rapImageButton);
+
+        filmImageButton = (ImageButton)v.findViewById(R.id.filmImageButton);
+        editImageButton = (ImageButton)v.findViewById(R.id.editImageButton);
+
+        japaneseImageButton = (ImageButton)v.findViewById(R.id.japaneseImageButton);
+        mathematicsImageButton = (ImageButton)v.findViewById(R.id.mathematicsImageButton);
+        englishImageButton = (ImageButton)v.findViewById(R.id.englishImageButton);
+        scienceImageButton = (ImageButton)v.findViewById(R.id.scienceImageButton);
+        societyImageButton = (ImageButton)v.findViewById(R.id.societyImageButton);
+        historyImageButton = (ImageButton)v.findViewById(R.id.historyImageButton);
 
 
         return v;
@@ -44,33 +94,246 @@ public class AreaFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         MainActivity.mToolbar.setTitle("探す");
+        sportsGroup.setVisibility(View.GONE);
+        musicGroup.setVisibility(View.GONE);
+        movieGroup.setVisibility(View.GONE);
+        studyGroup.setVisibility(View.GONE);
 
 
         sportsImageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                SportsTypeFragment fragmentSportsType = new SportsTypeFragment();
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container, fragmentSportsType, SportsTypeFragment.TAG)
-                        .commit();
+                areaGroup.setVisibility(View.GONE);
+                sportsGroup.setVisibility(View.VISIBLE);
             }
         });
         musicImageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                areaGroup.setVisibility(View.GONE);
+                musicGroup.setVisibility(View.VISIBLE);
+
             }
         });
         movieImageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                areaGroup.setVisibility(View.GONE);
+                movieGroup.setVisibility(View.VISIBLE);
             }
         });
         studyImageButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
+                areaGroup.setVisibility(View.GONE);
+                studyGroup.setVisibility(View.VISIBLE);
             }
         });
 
+        tennisImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        soccerImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","サッカー");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        athleticsImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","陸上");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        swimImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","水泳");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+
+        singImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        instrumentImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        vocalPercussionImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        rapImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+
+        filmImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+            }
+        });
+        editImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+
+        japaneseImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        mathematicsImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        englishImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        scienceImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        societyImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
+        historyImageButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Bundle areaBundle = new Bundle();
+                areaBundle.putString("type","テニス");
+
+                GridFragment fragmentGrid = new GridFragment();
+                fragmentGrid.setArguments(areaBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentGrid,GridFragment.TAG)
+                        .commit();
+            }
+        });
 
 
     }
