ea8bd8c4c874f72b3c0d6d96282c59c0daba8c4d
T014 <t014.ty@ex.fyf-h.com>
2018-08-17 09:36:53 +0900

    ドロワー追加 profileActivity profileのfragment作成中 アイコン画像ヘッダー画像読み込んだ プロフィールデータを送信 アクションバーの切り替え ドロワーの実装 ドロワーメニューの追加 ログアウト画面実装 利用規約画面実装 searchButton追加 データの取得できた プロフィール画像取得 投稿分野単一選択 投稿送信 タイムライン追加 分野分imagebutton配置 detailsfragment追加 searchfragment追加」 ユーザーデータ、ポストデータ変更 searchfragmentの条件分岐 検索機能拡張 タイムライン実装 follow,follower follow,follower開いたときにドロワー閉じる follow,follower数表示 検索結果表示 follow,follower調整中
    投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
    投稿分野改行
    AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
    データ保存上書きから更新に
    アカウント作成時にperiodに日時を入れる
    アカウントを表示するときはツールバーを非表示

---
+++ app/src/main/java/com/example/tyanai/myteacher2/BusinessData.java
@@ -0,0 +1,28 @@
+package com.example.tyanai.myteacher2;
+
+public class BusinessData {
+    private String mBought;
+    private String mCancel;
+    private String mDate;
+    private String mReceiveDate;
+    private String mSold;
+    private String mTradeDate;
+    private String mTradeKey;
+
+
+    public String getBought(){
+        return mBought;
+    }
+    public String getCancel(){
+        return mCancel;
+    }
+    public String getDate(){
+        return mDate;
+    }
+    public String getReceiveDate(){
+        return mReceiveDate;
+    }
+    public String getSold(){
+        return mSold;
+    }
+}
