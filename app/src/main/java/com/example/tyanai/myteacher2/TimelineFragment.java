2211a629f6a9b33fa60c6762000767261d2b11f6
T014 <t014.ty@ex.fyf-h.com>
2018-08-21 16:10:05 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了                       いいねリアルタイム更新
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/TimelineFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/TimelineFragment.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 
 public class TimelineFragment extends Fragment {
     public static  final String TAG = "TimelineFragment";
@@ -113,6 +114,47 @@ public class TimelineFragment extends Fragment {
         }
         @Override
         public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            String userId = (String) map.get("userId");
+            String userName = (String) map.get("userName");
+            String time = (String) map.get("time");
+            String key = (String) map.get("key");
+            String date = (String) map.get("date");
+            String imageBitmapString = (String) map.get("imageBitmapString");
+            String contents = (String) map.get("contents");
+            String cost = (String) map.get("cost");
+            String howLong = (String) map.get("howLong");
+            String goods = (String) map.get("goods");
+            String share = (String) map.get("share");
+            String bought = (String) map.get("bought");
+            String evaluation = (String) map.get("evaluation");
+            String cancel = (String) map.get("cancel");
+            String method = (String) map.get("method");
+            String postArea = (String) map.get("postArea");
+            String postType = (String) map.get("postType");
+            String level = (String) map.get("level");
+            String career = (String) map.get("career");
+            String place = (String) map.get("place");
+            String sex = (String) map.get("sex");
+            String age = (String) map.get("age");
+            String taught = (String) map.get("taught");
+            String userEvaluation = (String) map.get("userEvaluation");
+            String userIconBitmapString = (String) map.get("userIconBitmapString");
+
+
+
+            PostData postData = new PostData(userId,userName,time,key,date,imageBitmapString
+                    , contents,cost,howLong,goods,share,bought,evaluation,cancel,method,postArea
+                    , postType,level,career,place,sex,age,taught,userEvaluation,userIconBitmapString);
+
+            Collections.reverse(timeLineArrayList);
+            timeLineArrayList.add(postData);
+            Collections.reverse(timeLineArrayList);
+
+            mAdapter.setTimeLineArrayList(timeLineArrayList);
+            timeLineListView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
         }
         @Override
         public void onChildRemoved(DataSnapshot dataSnapshot) {
@@ -148,26 +190,40 @@ public class TimelineFragment extends Fragment {
         //followRef.addChildEventListener(fEventListener);
         contentsRef = mDataBaseReference.child(Const.ContentsPATH);
         contentsRef.limitToLast(5).addChildEventListener(tEventListener);
-        //取得数の上限を100から50に制限
+        //取得数の上限を5から50に制限上のreverseも
 
         timeLineListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
                 if (view.getId()==R.id.goodButton){
-                    String aaa="1";
-              }
+                    //いいねの処理
 
-                Bundle bundle = new Bundle();
+                    int totalGoods = Integer.parseInt(timeLineArrayList.get(position).getGood());
+                    totalGoods =totalGoods+1;
+                    String totalGd =String.valueOf(totalGoods);
 
-                bundle.putString("key",timeLineArrayList.get(position).getKey());
+                    timeLineArrayList.remove(timeLineArrayList.get(position));
+
+                    Map<String,Object> postGoodKey = new HashMap<>();
+                    postGoodKey.put("goods",totalGd);
+                    contentsRef.child(timeLineArrayList.get(position).getKey()).updateChildren(postGoodKey);
 
 
-                DetailsFragment fragmentDetails = new DetailsFragment();
-                fragmentDetails.setArguments(bundle);
-                getFragmentManager().beginTransaction()
-                        .replace(R.id.container,fragmentDetails,DetailsFragment.TAG)
-                        .commit();
+                }else{
+
+                    Bundle bundle = new Bundle();
+
+                    bundle.putString("key",timeLineArrayList.get(position).getKey());
+
+
+                    DetailsFragment fragmentDetails = new DetailsFragment();
+                    fragmentDetails.setArguments(bundle);
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.container,fragmentDetails,DetailsFragment.TAG)
+                            .commit();
+
+                }
 
             }
         });
