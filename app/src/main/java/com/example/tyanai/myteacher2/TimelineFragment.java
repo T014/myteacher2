352c4f9a44d697a767f7f0d2560ebbcc60dda131
T014 <t014.ty@ex.fyf-h.com>
2018-08-21 15:39:20 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/TimelineFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/TimelineFragment.java
@@ -9,6 +9,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -153,6 +154,9 @@ public class TimelineFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+                if (view.getId()==R.id.goodButton){
+                    String aaa="1";
+                }
 
                 Bundle bundle = new Bundle();
 
