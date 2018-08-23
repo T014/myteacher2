0897cc43a8cc5c77278118bb85007754e921c43a
T014 <t014.ty@ex.fyf-h.com>
2018-08-23 17:19:54 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了
                          messageはアイテムに沿ってアダプターを変更するだけ
    
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/ConfirmProfileFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/ConfirmProfileFragment.java
@@ -14,11 +14,13 @@ import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.util.Base64;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -41,6 +43,7 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
     TextView userNameTextView;
     TextView commentTextView;
     Button okButton;
+    Button messageButton;
     TextView sexTextView;
     TextView ageTextView;
     FirebaseUser user;
@@ -48,6 +51,9 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
     DatabaseReference mDataBaseReference;
     DatabaseReference followRef;
     DatabaseReference followerRef;
+    DatabaseReference contentsRef;
+    DatabaseReference messageRef;
+    DatabaseReference messageKeyRef;
     int followCount;
     int followerCount;
 
@@ -57,8 +63,69 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
     String uid;
     private Button followEditButton;
     private ArrayList<String> followArrayList;
+    private ArrayList<PostData> timeLineArrayList;
+    ListAdapter mAdapter;
+    ListView postListView;
 
 
+
+    private ChildEventListener updEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            String userId = (String) map.get("userId");
+            String userName = (String) map.get("userName");
+            String time = (String) map.get("time");
+            String key = (String) map.get("key");
+            String date = (String) map.get("date");
+            String imageBitmapString = (String) map.get("imageBitmapString");
+            String contents = (String) map.get("contents");
+            String cost = (String) map.get("cost");
+            String howLong = (String) map.get("howLong");
+            String goods = (String) map.get("goods");
+            String share = (String) map.get("share");
+            String bought = (String) map.get("bought");
+            String evaluation = (String) map.get("evaluation");
+            String cancel = (String) map.get("cancel");
+            String method = (String) map.get("method");
+            String postArea = (String) map.get("postArea");
+            String postType = (String) map.get("postType");
+            String level = (String) map.get("level");
+            String career = (String) map.get("career");
+            String place = (String) map.get("place");
+            String sex = (String) map.get("sex");
+            String age = (String) map.get("age");
+            String taught = (String) map.get("taught");
+            String userEvaluation = (String) map.get("userEvaluation");
+            String userIconBitmapString = (String) map.get("userIconBitmapString");
+
+
+
+            PostData postData = new PostData(userId,userName,time,key,date,imageBitmapString
+                    , contents,cost,howLong,goods,share,bought,evaluation,cancel,method,postArea
+                    , postType,level,career,place,sex,age,taught,userEvaluation,userIconBitmapString);
+
+            timeLineArrayList.add(postData);
+            mAdapter.setTimeLineArrayList(timeLineArrayList);
+            postListView.setAdapter(mAdapter);
+            mAdapter.notifyDataSetChanged();
+
+
+        }
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        }
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+        }
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+    };
 
     //mEventListenerの設定と初期化
     private ChildEventListener cEventListener = new ChildEventListener() {
@@ -87,47 +154,31 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
                     ,favorites,sex,age,evaluations,taught,period,groups,date,iconBitmapString,headerBitmapString);
 
 
-            if (user.getUid().equals(userData.getUid())){
-                followCount = Integer.parseInt(userData.getFollows());
-            }else if (intentUserId.equals(userData.getUid())){
-                followerCount = Integer.parseInt(userData.getFollowers());
-            }
+
             if (intentUserId!=null){
-                //ある
-                uid=intentUserId;
-                if (uid.equals(user.getUid())){
-                    //自分-
-                    followEditButton.setText("編集");
-                    //フォローボタンと編集ボタンを同じボタンにして
-                    //uidが一致するか否かでテキストとリスナーを変えるtwitterみたいに
-                }else{
-                    //他の人
-                    followEditButton.setText("フォロー");
+                if (user.getUid().equals(userData.getUid())){
+                    followCount = Integer.parseInt(userData.getFollows());
+                }else if (intentUserId.equals(userData.getUid())){
+                    followerCount = Integer.parseInt(userData.getFollowers());
                 }
-            }else{
-                //ない自分-
-                uid=user.getUid();
-                followEditButton.setText("編集");
+            }
+
+            userNameTextView.setText(userData.getName());
+            commentTextView.setText(userData.getComment());
+            sexTextView.setText(sex);
+            ageTextView.setText(age);
+            byte[] headerBytes = Base64.decode(headerBitmapString,Base64.DEFAULT);
+            if(headerBytes.length!=0){
+                Bitmap headerBitmap = BitmapFactory.decodeByteArray(headerBytes,0, headerBytes.length).copy(Bitmap.Config.ARGB_8888,true);
+                newHeaderImageView.setImageBitmap(headerBitmap);
+            }
+            byte[] iconBytes = Base64.decode(iconBitmapString,Base64.DEFAULT);
+            if(iconBytes.length!=0){
+                Bitmap iconBitmap = BitmapFactory.decodeByteArray(iconBytes,0, iconBytes.length).copy(Bitmap.Config.ARGB_8888,true);
+                newIconImageView.setImageBitmap(iconBitmap);
             }
 
 
-
-            if(userData.getUid().equals(uid)){
-                userNameTextView.setText(userData.getName());
-                commentTextView.setText(userData.getComment());
-                sexTextView.setText(sex);
-                ageTextView.setText(age);
-                byte[] headerBytes = Base64.decode(headerBitmapString,Base64.DEFAULT);
-                if(headerBytes.length!=0){
-                    Bitmap headerBitmap = BitmapFactory.decodeByteArray(headerBytes,0, headerBytes.length).copy(Bitmap.Config.ARGB_8888,true);
-                    newHeaderImageView.setImageBitmap(headerBitmap);
-                }
-                byte[] iconBytes = Base64.decode(iconBitmapString,Base64.DEFAULT);
-                if(iconBytes.length!=0){
-                    Bitmap iconBitmap = BitmapFactory.decodeByteArray(iconBytes,0, iconBytes.length).copy(Bitmap.Config.ARGB_8888,true);
-                    newIconImageView.setImageBitmap(iconBitmap);
-                }
-            }
         }
 
         @Override
@@ -149,8 +200,20 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
         public void onChildAdded(DataSnapshot dataSnapshot, String s) {
             HashMap map = (HashMap) dataSnapshot.getValue();
 
-            String followUid = (String) map.get("followerUid");
+            String followUid = (String) map.get("followUid");
             followArrayList.add(followUid);
+
+            if (intentUserId!=null){
+                if (!(intentUserId.equals(user.getUid()))){
+                    if (followArrayList.contains(uid)){
+                        followEditButton.setText("フォロー中");
+                    }else{
+                        followEditButton.setText("フォロー");
+                    }
+                }
+            }
+
+
 
         }
         @Override
@@ -171,11 +234,21 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
+
         followArrayList = new ArrayList<String>();
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         mDataBaseReference = FirebaseDatabase.getInstance().getReference();
         followRef = mDataBaseReference.child(Const.FollowPATH).child(user.getUid());
+        contentsRef = mDataBaseReference.child(Const.ContentsPATH);
+        messageRef = mDataBaseReference.child(Const.MessagePATH);
+        messageKeyRef = mDataBaseReference.child(Const.MessageKeyPATH);
+
+
+
         followRef.addChildEventListener(followEventListener);
+
+
     }
 
 
@@ -199,6 +272,8 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
         sexTextView = (TextView)v.findViewById(R.id.sexTextView);
         ageTextView = (TextView)v.findViewById(R.id.ageTextView);
         followEditButton = (Button)v.findViewById(R.id.followEditButton);
+        postListView = (ListView)v.findViewById(R.id.postListView);
+        messageButton = (Button)v.findViewById(R.id.messageButton);
 
 
         return v;
@@ -208,32 +283,45 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
         super.onViewCreated(view, savedInstanceState);
 
 
+
         //画像とテキストを引っ張ってくる
         //mDataBaseReference = FirebaseDatabase.getInstance().getReference();
         //user = FirebaseAuth.getInstance().getCurrentUser();
         userRef = mDataBaseReference.child(Const.UsersPATH);
         //followRef = mDataBaseReference.child(Const.FollowPATH);
         followerRef  = mDataBaseReference.child(Const.FollowerPATH);
-
+        mAdapter = new ListAdapter(this.getActivity(),R.layout.list_item);
+        timeLineArrayList = new ArrayList<PostData>();
 
         Bundle userBundle = getArguments();
         if (userBundle!=null){
             intentUserId = userBundle.getString("userId");
         }
+        if (intentUserId!=null){
+            //ある
+            uid=intentUserId;
+            if (uid.equals(user.getUid())){
+                //自分-
+                followEditButton.setText("編集");
+            }
+        }else{
+            //ない自分-
+            uid=user.getUid();
+            followEditButton.setText("編集");
+        }
+
+        contentsRef.orderByChild("userId").equalTo(user.getUid()).addChildEventListener(updEventListener);
 
 
-
-
-        //userRef.child(uid).addChildEventListener(pEventListener);
-        userRef.addChildEventListener(cEventListener);
+        userRef.orderByChild("userId").equalTo(uid).addChildEventListener(cEventListener);
 
         final String[] pageTitle = {"投稿", "いいね"};
 
 
-        ProfilePostGoodFragment fragmentProfilePostGood = new ProfilePostGoodFragment();
-        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-        transaction.add(R.id.tabContainer, fragmentProfilePostGood);
-        transaction.commit();
+//        ProfilePostGoodFragment fragmentProfilePostGood = new ProfilePostGoodFragment();
+//        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+//        transaction.add(R.id.tabContainer, fragmentProfilePostGood);
+//        transaction.commit();
 
         FragmentPagerAdapter adapter = new FragmentPagerAdapter(getActivity().getSupportFragmentManager()) {
             @Override
@@ -277,7 +365,7 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
                     followData.put("followUid",intentUserId);
                     Map<String,Object> childUpdates = new HashMap<>();
                     childUpdates.put(key,followData);
-                    followRef.child(user.getUid()).updateChildren(childUpdates);
+                    followRef.updateChildren(childUpdates);
 
                     //フォロー数追加
                     Map<String,Object> plusFollowCount = new HashMap<>();
@@ -309,26 +397,74 @@ public class ConfirmProfileFragment extends Fragment implements ViewPager.OnPage
             }
         });
 
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                Map<String,Object> makeMessageKeyRef = new HashMap<>();
+                String key = messageRef.push().getKey();
+                makeMessageKeyRef.put("messageKey",key);
+                makeMessageKeyRef.put("uid",user.getUid());
+                Map<String,Object> childUpdates = new HashMap<>();
+                childUpdates.put(key,makeMessageKeyRef);
+                messageKeyRef.child(intentUserId).updateChildren(childUpdates);
+
+
+
+                Map<String,Object> makeMessageKeyRef2 = new HashMap<>();
+                makeMessageKeyRef2.put("messageKey",key);
+                makeMessageKeyRef2.put("uid",intentUserId);
+                Map<String,Object> childUpdates2 = new HashMap<>();
+                childUpdates2.put(key,makeMessageKeyRef2);
+                messageKeyRef.child(user.getUid()).updateChildren(childUpdates2);
+
+
+
+                Map<String,Object> makeMessageRef = new HashMap<>();
+                makeMessageRef.put("uid","");
+                makeMessageRef.put("userName","");
+                makeMessageRef.put("iconBitmapString","");
+                makeMessageRef.put("bitmapString","");
+                makeMessageRef.put("contents","");
+                makeMessageRef.put("time","");
+                Map<String,Object> childUp = new HashMap<>();
+                childUp.put(key,makeMessageRef);
+                messageRef.child(key).updateChildren(childUp);
+
+
+
+
+                MessageFragment fragmentMessage = new MessageFragment();
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentMessage,MessageFragment.TAG)
+                        .commit();
+            }
+        });
+
 
 
     }
+
+
 
     @Override
     public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
 
 
-        int tabPosition = tabLayout.getSelectedTabPosition();
-        if (tabPosition==1){
-            //goodグリッドリストでいいかな
 
-        }else if(tabPosition==0){
-            //post
-        }
     }
 
     @Override
     public void onPageSelected(int position) {
         //同上
+        int tabPosition = tabLayout.getSelectedTabPosition();
+        if (tabPosition==1){
+            //goodグリッドリストでいいかな
+        }else if(tabPosition==0){
+            //post
+            contentsRef.orderByChild("userId").equalTo(user.getUid()).addChildEventListener(updEventListener);
+        }
     }
 
     @Override
