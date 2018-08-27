92abd02a4c243dc87290a91a33e605ec31c3e720
T014 <t014.ty@ex.fyf-h.com>
2018-08-27 11:50:42 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了
                          messageはアイテムに沿ってアダプターを変更するだけ
    
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/NotificationFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/NotificationFragment.java
@@ -1,7 +1,12 @@
 package com.example.tyanai.myteacher2;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,8 +16,14 @@ import com.google.firebase.auth.FirebaseAuth;
 
 //activityloginのボタンとかテキストを非表示にする
 
-public class NotificationFragment extends Fragment {
+public class NotificationFragment extends Fragment implements ViewPager.OnPageChangeListener,
+        NotificationPageFragment.OnFragmentInteractionListener {
     public static final String TAG = "NotificationFragment";
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    FragmentStatePagerAdapter adapter;
+
 
 
     @Override
@@ -21,14 +32,57 @@ public class NotificationFragment extends Fragment {
         View v = inflater.inflate(R.layout.fragment_notification,container,false);
 
 
+        tabLayout = (TabLayout) v.findViewById(R.id.tabs);
+        viewPager = (ViewPager) v.findViewById(R.id.pager);
 
         return v;
     }
 
     public void onViewCreated(View view,Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
         MainActivity.mToolbar.setTitle("通知");
 
+        final String[] pageTitle = {"HOME", "EVENT", "SETTING"};
+
+        adapter = new FragmentStatePagerAdapter(getActivity().getSupportFragmentManager()) {
+            @Override
+            public Fragment getItem(int position) {
+                return NotificationPageFragment.newInstance(position + 1);
+            }
+
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return pageTitle[position];
+            }
+
+            @Override
+            public int getCount() {
+                return pageTitle.length;
+            }
+        };
+
+        // ViewPagerにページを設定
+        viewPager.setAdapter(adapter);
+        viewPager.addOnPageChangeListener(this);
+
+        // ViewPagerをTabLayoutを設定
+        tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
     }
 }
+
