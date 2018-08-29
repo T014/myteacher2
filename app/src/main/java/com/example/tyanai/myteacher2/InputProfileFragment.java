459e84cac2f0439f02f944625231c1e850de63cd
T014 <t014.ty@ex.fyf-h.com>
2018-08-29 17:12:37 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了
                          messageはアイテムに沿ってアダプターを変更するだけ
    
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/InputProfileFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/InputProfileFragment.java
@@ -42,7 +42,18 @@ public class InputProfileFragment extends Fragment {
     FirebaseUser user;
     DatabaseReference userRef;
     DatabaseReference mDataBaseReference;
-
+    DatabaseReference usersContentsRef;
+    DatabaseReference contentsRef;
+    String myFollows;
+    String myFollowers;
+    String myPosts;
+    String myEvaluation;
+    String myTaught;
+    String myPeriod;
+    String myGroup;
+    String myFavorite;
+    String newUserName;
+    String newIconBitmapString;
 
 
 
@@ -71,47 +82,142 @@ public class InputProfileFragment extends Fragment {
             UserData userData = new UserData(userName,userId,comment,follows,followers,posts
                     ,favorites,sex,age,evaluations,taught,period,groups,date,iconBitmapString,headerBitmapString);
 
-            if(userData.getUid().equals(user.getUid())) {
-                if (sex.equals("未設定")){
-                    sexSpinner.setSelection(0);
-                }else if (sex.equals("男性")){
-                    sexSpinner.setSelection(1);
-                }else if(sex.equals("女性")){
-                    sexSpinner.setSelection(2);
-                }else if (sex.equals("中性")){
-                    sexSpinner.setSelection(3);
-                }
-                if (age.equals("未設定")){
-                    ageSpinner.setSelection(0);
-                }else if (age.equals("60代")){
-                    ageSpinner.setSelection(1);
-                }else if (age.equals("50代")){
-                    ageSpinner.setSelection(2);
-                }else if (age.equals("40代")){
-                    ageSpinner.setSelection(3);
-                }else if (age.equals("30代")){
-                    ageSpinner.setSelection(4);
-                }else if (age.equals("20代")){
-                    ageSpinner.setSelection(5);
-                }else if (age.equals("10代")){
-                    ageSpinner.setSelection(6);
-                }
-                startDate = userData.getDate();
-                userNameEditText.setText(userData.getName());
-                commentEditText.setText(userData.getComment());
-                byte[] headerBytes = Base64.decode(headerBitmapString, Base64.DEFAULT);
-                if (headerBytes.length != 0) {
-                    Bitmap headerBitmap = BitmapFactory.decodeByteArray(headerBytes, 0, headerBytes.length).copy(Bitmap.Config.ARGB_8888, true);
-                    headerImageView.setImageBitmap(headerBitmap);
-                }
-                byte[] iconBytes = Base64.decode(iconBitmapString, Base64.DEFAULT);
-                if (iconBytes.length != 0) {
-                    Bitmap iconBitmap = BitmapFactory.decodeByteArray(iconBytes, 0, iconBytes.length).copy(Bitmap.Config.ARGB_8888, true);
-                    iconImageView.setImageBitmap(iconBitmap);
-                }
+            myFollows=follows;
+            myFollowers=followers;
+            myPosts=posts;
+            myEvaluation=evaluations;
+            myPeriod=period;
+            myTaught=taught;
+            myGroup=groups;
+            myFavorite=favorites;
+            if (sex.equals("未設定")){
+                sexSpinner.setSelection(0);
+            }else if (sex.equals("男性")){
+                sexSpinner.setSelection(1);
+            }else if(sex.equals("女性")){
+                sexSpinner.setSelection(2);
+            }else if (sex.equals("中性")){
+                sexSpinner.setSelection(3);
+            }
+            if (age.equals("未設定")){
+                ageSpinner.setSelection(0);
+            }else if (age.equals("60代")){
+                ageSpinner.setSelection(1);
+            }else if (age.equals("50代")){
+                ageSpinner.setSelection(2);
+            }else if (age.equals("40代")){
+                ageSpinner.setSelection(3);
+            }else if (age.equals("30代")){
+                ageSpinner.setSelection(4);
+            }else if (age.equals("20代")){
+                ageSpinner.setSelection(5);
+            }else if (age.equals("10代")){
+                ageSpinner.setSelection(6);
+            }
+            startDate = userData.getDate();
+            userNameEditText.setText(userData.getName());
+            commentEditText.setText(userData.getComment());
+            byte[] headerBytes = Base64.decode(headerBitmapString, Base64.DEFAULT);
+            if (headerBytes.length != 0) {
+                Bitmap headerBitmap = BitmapFactory.decodeByteArray(headerBytes, 0, headerBytes.length).copy(Bitmap.Config.ARGB_8888, true);
+                headerImageView.setImageBitmap(headerBitmap);
+            }
+            byte[] iconBytes = Base64.decode(iconBitmapString, Base64.DEFAULT);
+            if (iconBytes.length != 0) {
+                Bitmap iconBitmap = BitmapFactory.decodeByteArray(iconBytes, 0, iconBytes.length).copy(Bitmap.Config.ARGB_8888, true);
+                iconImageView.setImageBitmap(iconBitmap);
             }
         }
 
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+        }
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+        }
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        }
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
+    };
+
+
+    private ChildEventListener tEventListener = new ChildEventListener() {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            HashMap map = (HashMap) dataSnapshot.getValue();
+
+            String userId = (String) map.get("userId");
+            String userName = (String) map.get("userName");
+            String time = (String) map.get("time");
+            String key = (String) map.get("key");
+            String date = (String) map.get("date");
+            String imageBitmapString = (String) map.get("imageBitmapString");
+            String contents = (String) map.get("contents");
+            String cost = (String) map.get("cost");
+            String howLong = (String) map.get("howLong");
+            String goods = (String) map.get("goods");
+            String share = (String) map.get("share");
+            String bought = (String) map.get("bought");
+            String evaluation = (String) map.get("evaluation");
+            String cancel = (String) map.get("cancel");
+            String method = (String) map.get("method");
+            String postArea = (String) map.get("postArea");
+            String postType = (String) map.get("postType");
+            String level = (String) map.get("level");
+            String career = (String) map.get("career");
+            String place = (String) map.get("place");
+            String sex = (String) map.get("sex");
+            String age = (String) map.get("age");
+            String taught = (String) map.get("taught");
+            String userEvaluation = (String) map.get("userEvaluation");
+            String userIconBitmapString = (String) map.get("userIconBitmapString");
+
+
+
+            Map<String,Object> data = new HashMap<>();
+
+
+            data.put("userId", userId);
+            data.put("userName",newUserName);
+            data.put("time", time);
+            data.put("key", key);
+            data.put("date", date);
+            data.put("imageBitmapString", imageBitmapString);
+            data.put("contents",contents );
+            data.put("cost",cost );
+            data.put("howLong", howLong);
+            data.put("goods",goods );
+            data.put("share",share );
+            data.put("bought",bought );
+            data.put("evaluation", evaluation);
+            data.put("cancel",cancel );
+            data.put("method", method);
+            data.put("postArea", postArea);
+            data.put("postType",postType );
+            data.put("level",level );
+            data.put("career", career);
+            data.put("place",place);
+            data.put("sex",sex);
+            data.put("age",age);
+            data.put("taught",taught);
+            data.put("userEvaluation",userEvaluation);
+            data.put("userIconBitmapString",newIconBitmapString);
+
+
+
+            Map<String,Object> childUpdates = new HashMap<>();
+            childUpdates.put(key,data);
+            contentsRef.updateChildren(childUpdates);
+
+
+
+
+
+
+        }
         @Override
         public void onChildChanged(DataSnapshot dataSnapshot, String s) {
         }
@@ -151,11 +257,19 @@ public class InputProfileFragment extends Fragment {
     public void onViewCreated(View view,Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        MainActivity.bottomNavigationView.setVisibility(View.GONE);
+        MainActivity.mToolbar.setVisibility(View.GONE);
+
         mDataBaseReference = FirebaseDatabase.getInstance().getReference();
         user = FirebaseAuth.getInstance().getCurrentUser();
         userRef = mDataBaseReference.child(Const.UsersPATH);
+        usersContentsRef = mDataBaseReference.child(Const.UsersContentsPATH);
+        contentsRef = mDataBaseReference.child(Const.ContentsPATH);
+        userRef.orderByChild("userId").equalTo(user.getUid()).addChildEventListener(iEventListener);
 
-        userRef.addChildEventListener(iEventListener);
+
+
+
 
 
 
@@ -206,7 +320,7 @@ public class InputProfileFragment extends Fragment {
 
 
                 //画像とテキストをデータベースに送信
-                String userName = userNameEditText.getText().toString();
+                newUserName = userNameEditText.getText().toString();
                 String comment = commentEditText.getText().toString();
 
                 //2回同じ処理をしてるから効率化できそう
@@ -246,78 +360,53 @@ public class InputProfileFragment extends Fragment {
 
                 ByteArrayOutputStream iconBaos = new ByteArrayOutputStream();
                 iconResizedImage.compress(Bitmap.CompressFormat.JPEG, 80, iconBaos);
-                String iconBitmapString = Base64.encodeToString(iconBaos.toByteArray(), Base64.DEFAULT);
+                newIconBitmapString = Base64.encodeToString(iconBaos.toByteArray(), Base64.DEFAULT);
 
 
                 userRef = mDataBaseReference.child(Const.UsersPATH);
 
                 String userId = user.getUid();
 
-
-
-
-
-                String follows = "0";
-                String followers = "0";
-                String posts = "0";
-                String evaluations = "0";
-                String taught = "0";
-                String period = "0";
-                String groups = "0";
-                String favorites = "未設定";
                 String sex = (String)sexSpinner.getSelectedItem();
                 String age = (String)ageSpinner.getSelectedItem();
 
-
-
-
-
                 //Firebaseにデータ作成、データのkey取得
-
-                //Map<String,String> data = new HashMap<>();
-                //objectでないと更新できない
                 Map<String,Object> data = new HashMap<>();
-
-
                 //データベースへの書き方の確認
 
-                data.put("userName", userName);
+                data.put("userName", newUserName);
                 data.put("userId", userId);
                 data.put("comment", comment);
 
-
-
-
-                data.put("follows", follows);
-                data.put("followers", followers);
-                data.put("posts", posts);
-                data.put("favorites",favorites);
+                data.put("follows", myFollows);
+                data.put("followers", myFollowers);
+                data.put("posts", myPosts);
+                data.put("favorites",myFavorite);
                 data.put("sex",sex);
-                data.put("age",age);                //評価
-                data.put("evaluations", evaluations);
+                data.put("age",age);
+                //評価
+                data.put("evaluations", myEvaluation);
                 //指導人数
-                data.put("taught", taught);
+                data.put("taught", myTaught);
                 //アプリ使用期間
-                data.put("period", period);
+                data.put("period", myPeriod);
                 //参加グループ数
-                data.put("groups", groups);
+                data.put("groups", myGroup);
                 data.put("date",startDate);
-
-
-
-
-
-                data.put("iconBitmapString", iconBitmapString);
+                data.put("iconBitmapString", newIconBitmapString);
                 data.put("headerBitmapString", headerBitmapString);
 
                 Map<String,Object> childUpdates = new HashMap<>();
                 childUpdates.put(userId,data);
                 userRef.updateChildren(childUpdates);
 
-                //userRef.child(userId).setValue(data);
-                //setだとデータをつけるだけupdateだと任意の値だけを変更できる
-                //userRef.child(userId).updateChildren(data);
 
+
+                contentsRef.orderByChild("userId").equalTo(user.getUid()).addChildEventListener(tEventListener);
+
+
+                MainActivity.bottomNavigationView.setVisibility(View.VISIBLE);
+                MainActivity.mToolbar.setVisibility(View.VISIBLE);
 
 
                 ConfirmProfileFragment fragmentConfirmProfile = new ConfirmProfileFragment();
@@ -325,6 +414,8 @@ public class InputProfileFragment extends Fragment {
                 getFragmentManager().beginTransaction()
                         .replace(R.id.container, fragmentConfirmProfile, ConfirmProfileFragment.TAG)
                         .commit();
+
+
 
             }
         });
