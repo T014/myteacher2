831158a2722a8441b6ca9c2387cda706e59734f0
T014 <t014.ty@ex.fyf-h.com>
2018-09-12 16:50:54 +0900

    自分の端末ではmakepostのlinearlayout,buttonのどちらかにmarginを入れれば効果ある                       他端末では両方に入れないと効果なかった                       タイムライン修正                       タイムラインで自分のアイコンがコンテンツ画像になってたのを修正                       タイムラインの投稿を新しい順に指定                       プロフィール変更に伴うすべての投稿の変更完了
                          messageはアイテムに沿ってアダプターを変更するだけ
    
                          投稿画面の画像選択はprofileactivityをmainactivityに統合してからやる
                          投稿分野改行
                          AreaFragmentの後はリストで画像、テキストを持っていれば各分野のためのfragmentが不必要になる
                          データ保存上書きから更新に
                          アカウント作成時にperiodに日時を入れる
                          アカウントを表示するときはツールバーを非表示

--- app/src/main/java/com/example/tyanai/myteacher2/BusinessFragment.java
+++ app/src/main/java/com/example/tyanai/myteacher2/BusinessFragment.java
@@ -5,11 +5,16 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +23,16 @@ public class BusinessFragment extends Fragment {
     public static final String TAG = "BusinessFragment";
 
     ListView businessListView;
-    private ArrayList<BusinessData> businessDataArrayList = new ArrayList<BusinessData>();
-
+    private ArrayList<BusinessData> businessDataArrayList;
+    private BusinessDataListAdapter mAdapter;
+    DatabaseReference mDataBaseReference;
+    DatabaseReference tradeRef;
+    DatabaseReference requestRef;
+    FirebaseUser user;
+    public static String tradeKey;
+    String screenNum;
+    public static String boughtUid;
+    public static String buyDate;
 
     private ChildEventListener tEventListener = new ChildEventListener() {
         @Override
@@ -27,16 +40,48 @@ public class BusinessFragment extends Fragment {
             HashMap map = (HashMap) dataSnapshot.getValue();
 
             String bought = (String) map.get("bought");
-            String cancel = (String) map.get("cancel");
             String date = (String) map.get("date");
             String receiveDate = (String) map.get("receiveDate");
             String sold = (String) map.get("sold");
-            String tradeDate = (String) map.get("tradeDate");
+            String payDay = (String) map.get("payDay");
             String tradeKey = (String) map.get("tradeKey");
+            String userName = (String) map.get("userName");
+            String userIcon = (String) map.get("userIcon");
+            String evaluation = (String) map.get("evaluation");
+            String judgment = (String) map.get("judgment");
+            String postKey = (String) map.get("postKey");
+            String contentImageBitmapString = (String) map.get("contentImageBitmapString");
+            String kind = (String) map.get("kind");
+            String kindDetail = (String) map.get("kindDetail");
+            String buyName = (String) map.get("buyName");
+            String buyIconBitmapString = (String) map.get("buyIconBitmapString");
+            String permittedDate = (String) map.get("permittedDate");
 
-            BusinessData businessData = new BusinessData(bought,cancel,date,receiveDate,sold,tradeDate,tradeKey);
-            
-            businessDataArrayList.add(businessData);
+
+            BusinessData businessData = new BusinessData(bought, date,receiveDate,sold,payDay,tradeKey, userName,userIcon,evaluation,
+                    judgment,postKey,contentImageBitmapString,kind,kindDetail,buyName,buyIconBitmapString,permittedDate);
+
+            if (screenNum.equals("request")){
+                if (businessData.getKindDetail().equals("リクエスト")){
+                    businessDataArrayList.add(businessData);
+                    mAdapter.setBusinessDataArrayList(businessDataArrayList);
+                    businessListView.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }else if (screenNum.equals("apply")){
+                if (businessData.getKindDetail().equals("リクエスト")){
+                    businessDataArrayList.add(businessData);
+                    mAdapter.setBusinessDataArrayList(businessDataArrayList);
+                    businessListView.setAdapter(mAdapter);
+                    mAdapter.notifyDataSetChanged();
+                }
+            }else if (screenNum.equals("business")){
+                businessDataArrayList.add(businessData);
+                mAdapter.setBusinessDataArrayList(businessDataArrayList);
+                businessListView.setAdapter(mAdapter);
+                mAdapter.notifyDataSetChanged();
+            }
+
 
         }
         @Override
@@ -67,8 +112,66 @@ public class BusinessFragment extends Fragment {
     public void onViewCreated(View view,Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        MainActivity.mToolbar.setTitle("取引履歴");
+        tradeKey = null;
+
+        businessDataArrayList = new ArrayList<BusinessData>();
+        mAdapter = new BusinessDataListAdapter(this.getActivity(),R.layout.business_item);
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        mDataBaseReference = FirebaseDatabase.getInstance().getReference();
+        tradeRef = mDataBaseReference.child(Const.TradePATH);
+        requestRef = mDataBaseReference.child(Const.RequestPATH);
+
+        Bundle screenBundle = getArguments();
+        if (screenBundle!=null){
+            screenNum = screenBundle.getString("screenKey");
+
+            if (screenNum.equals("business")){
+                MainActivity.mToolbar.setTitle("取引履歴");
+                tradeRef.orderByChild("bought").equalTo(user.getUid()).addChildEventListener(tEventListener);
+                tradeRef.orderByChild("sold").equalTo(user.getUid()).addChildEventListener(tEventListener);
+            }else if (screenNum.equals("apply")){
+                MainActivity.mToolbar.setTitle("取引申請");
+                requestRef.orderByChild("bought").equalTo(user.getUid()).addChildEventListener(tEventListener);
+            }else if (screenNum.equals("request")){
+                MainActivity.mToolbar.setTitle("取引リクエスト");
+                requestRef.orderByChild("sold").equalTo(user.getUid()).addChildEventListener(tEventListener);
+            }
+        }else{
+            tradeRef.orderByChild("bought").equalTo(user.getUid()).addChildEventListener(tEventListener);
+        }
+
+
+
+
+
+        businessListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public  void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String judge = businessDataArrayList.get(position).getEvaluation();
+
+                if (judge.equals("0")){
+                    tradeKey=businessDataArrayList.get(position).getTradeKey();
+                }
+
+                boughtUid = businessDataArrayList.get(position).getBought();
+                buyDate = businessDataArrayList.get(position).getDate();
+
+
+                Bundle businessBundle = new Bundle();
+
+                businessBundle.putString("key",businessDataArrayList.get(position).getPostKey());
+                businessBundle.putString("screenKey",screenNum);
+
+
+                DetailsFragment fragmentDetails = new DetailsFragment();
+                fragmentDetails.setArguments(businessBundle);
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container,fragmentDetails,DetailsFragment.TAG)
+                        .commit();
+            }
+        });
 
     }
+
 
 }
