28ab8284fe568c4df58139ff186f35a6c1c136e6
T014 <t014.ty@ex.fyf-h.com>
2018-10-02 17:27:54 +0900

    トリミング修正中

---
+++ app/src/main/java/com/example/tyanai/myteacher2/animation/SimpleValueAnimatorListener.java
@@ -0,0 +1,9 @@
+package com.example.tyanai.myteacher2.animation;
+
+public interface SimpleValueAnimatorListener {
+    void onAnimationStarted();
+
+    void onAnimationUpdated(float scale);
+
+    void onAnimationFinished();
+}
